b95f438ee337811012bcba2ce6c996e120b1533a
Priya <g.sai.swarajya.priya@accenture.com>
2025-09-02 12:28:11 +0530

    12345

--- project_planner/services.py
+++ project_planner/services.py
@@ -18,62 +18,62 @@ else:
 # api_key = os.getenv("AZURE_OPENAI_API_KEY")
 # endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 
-# chat_completion_service_1 = AzureChatCompletion(
-#     api_key=api_key,
-#     endpoint=endpoint,
-#     deployment_name="gpt-4o",
-#     service_id="gpt-4-service"
-# )
+chat_completion_service_1 = AzureChatCompletion(
+    api_key=api_key,
+    endpoint=endpoint,
+    deployment_name="gpt-4o",
+    service_id="gpt-4-service"
+)
 
-# chat_completion_service_2 = AzureChatCompletion(
-#     api_key=api_key,
-#     endpoint=endpoint,
-#     deployment_name="gpt-4o",
-#     service_id="gpt-4-service"
-# )
+chat_completion_service_2 = AzureChatCompletion(
+    api_key=api_key,
+    endpoint=endpoint,
+    deployment_name="gpt-4o",
+    service_id="gpt-4-service"
+)
 
 
-# chat_completion_service_1 = AzureChatCompletion(
-#     api_key=api_key,
-#     endpoint=endpoint,
-#     deployment_name="gpt-4o",
-#     service_id = "gpt-4-service"
-# )
+chat_completion_service_1 = AzureChatCompletion(
+    api_key=api_key,
+    endpoint=endpoint,
+    deployment_name="gpt-4o",
+    service_id = "gpt-4-service"
+)
 
-# chat_completion_service_2 = AzureChatCompletion(
-#     api_key=api_key,
-#     endpoint=endpoint,
-#     deployment_name="gpt-4o",
-#     service_id = "gpt-4-service"
-# )
+chat_completion_service_2 = AzureChatCompletion(
+    api_key=api_key,
+    endpoint=endpoint,
+    deployment_name="gpt-4o",
+    service_id = "gpt-4-service"
+)
 
-# chat_completion_service_3 = AzureChatCompletion(
-#     api_key=api_key,
-#     endpoint=endpoint,
-#     deployment_name="gpt-4o",
-#     service_id = "gpt-4-service"
-# )
+chat_completion_service_3 = AzureChatCompletion(
+    api_key=api_key,
+    endpoint=endpoint,
+    deployment_name="gpt-4o",
+    service_id = "gpt-4-service"
+)
 
-# chat_completion_service_4 = AzureChatCompletion(
-#     api_key=api_key,
-#     endpoint=endpoint,
-#     deployment_name="gpt-4o",
-#     service_id = "gpt-4-service"
-# )
+chat_completion_service_4 = AzureChatCompletion(
+    api_key=api_key,
+    endpoint=endpoint,
+    deployment_name="gpt-4o",
+    service_id = "gpt-4-service"
+)
 
-# chat_completion_service_5 = AzureChatCompletion(
-#     api_key=api_key,
-#     endpoint=endpoint,
-#     deployment_name="gpt-4o",
-#     service_id = "gpt-4-service"
-# )
+chat_completion_service_5 = AzureChatCompletion(
+    api_key=api_key,
+    endpoint=endpoint,
+    deployment_name="gpt-4o",
+    service_id = "gpt-4-service"
+)
 
-# chat_completion_service_6 = AzureChatCompletion(
-#     api_key=api_key,
-#     endpoint=endpoint,
-#     deployment_name="gpt-4o",
-#     service_id = "gpt-4-service"
-# )
+chat_completion_service_6 = AzureChatCompletion(
+    api_key=api_key,
+    endpoint=endpoint,
+    deployment_name="gpt-4o",
+    service_id = "gpt-4-service"
+)
 
 #2222
 # import os
